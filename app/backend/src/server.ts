09237daadf33de66658c205749c65ec4328ea540
valdeci <moura97.valdeci@gmail.com>
2022-08-25 18:35:18 -0300

    refactor(prisma user model): add unique index at email field

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,5 +1,21 @@
 import App from './app';
+import CustomRouter from './routes';
+import UserController from './controllers/UserController';
+
+const userController = new UserController();
+const userRouter = new CustomRouter();
+
+userRouter.addGetRoute(userController.route, userController.read);
+userRouter.addGetRoute(`${userController.route}/:id`, userController.readOne);
+userRouter.addPostRoute(userController.route, userController.create);
+userRouter.addPatchRoute(
+  `${userController.route}/:id/update-profile`,
+  userController.update
+);
+userRouter.addDeleteRoute(`${userController.route}/:id`, userController.delete);
 
 const server = new App();
+
+server.addRouter(userRouter.router);
 
 export default server;
