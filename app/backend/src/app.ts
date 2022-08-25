27326947a58fa558c33cf91cd1a582fd00860adb
valdeci <moura97.valdeci@gmail.com>
2022-08-25 15:32:46 -0300

    feat(app): create main class

---
+++ app/backend/src/app.ts
@@ -0,0 +1,31 @@
+import express, { Router, ErrorRequestHandler } from 'express';
+import cors from 'cors';
+import GlobalErrorHandler from './middlewares';
+
+export default class App {
+  private app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
+
+  public start(PORT: number | string = 3001): void {
+    this.app.listen(() => console.log('Server runing at port', PORT));
+  }
+
+  public addRouter(router: Router): void {
+    this.app.use(router);
+  }
+
+  public getApp(): express.Application {
+    return this.app;
+  }
+
+  public addErrorMiddleware(
+    middleware: ErrorRequestHandler = new GlobalErrorHandler().error
+  ): void {
+    this.app.use(middleware);
+  }
+}
