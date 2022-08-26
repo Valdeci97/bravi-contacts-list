70e4f2ba3120d032060641553012360bcded828a
valdeci <moura97.valdeci@gmail.com>
2022-08-26 16:39:09 -0300

    refactor(joi schemas): add empty string validation

--- app/backend/src/utils/joi-schemas/contact.ts
+++ app/backend/src/utils/joi-schemas/contact.ts
@@ -7,7 +7,7 @@ const BOOLEAN_BASE = '400/Whatsapp field must be boolean';
 const UNFILLED_EMAIL = '400/Email field must be filled';
 const INVALID_EMAIL =
   '400/Email field must follow the email pattern. e.g user@user.com';
-const STRING_BASE = '400/This field must be a string';
+const STRING_BASE = '400/{{#label}} field must be a string';
 
 export const contactPhone = joi
   .object({
--- app/backend/src/utils/joi-schemas/user.ts
+++ app/backend/src/utils/joi-schemas/user.ts
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const UNFILLED_NAME = '400/Name field must be filled';
 const MIN_LENGTH_NAME = '400/Name field must have at leats 3 characters length';
-const STRING_BASE = '400/This field must be a string';
+const STRING_BASE = '400/{{#label}} field must be a string';
 const UNFILLED_EMAIL = '400/Email field must be filled';
 const INVALID_EMAIL =
   '400/Email field must follow the email pattern. e.g user@user.com';
@@ -14,7 +14,7 @@ export const username = joi
   .object({
     name: joi.string().min(3).required().messages({
       'any.required': UNFILLED_NAME,
-      'string. empty': UNFILLED_NAME,
+      'string.empty': UNFILLED_NAME,
       'string.min': MIN_LENGTH_NAME,
       'string.base': STRING_BASE,
     }),
@@ -25,6 +25,7 @@ export const userEmail = joi
   .object({
     email: joi.string().email().required().messages({
       'any.required': UNFILLED_EMAIL,
+      'string.empty': UNFILLED_EMAIL,
       'string.email': INVALID_EMAIL,
       'string.base': STRING_BASE,
     }),
@@ -46,6 +47,7 @@ export const userOptionalEmail = joi
   .object({
     email: joi.string().email().messages({
       'string.email': INVALID_EMAIL,
+      'string.empty': UNFILLED_EMAIL,
       'string.base': STRING_BASE,
     }),
   })
