2adaa95f639be1fe297f2c54caafaf2462bd2209
valdeci <moura97.valdeci@gmail.com>
2022-08-25 22:06:06 -0300

    feat(contact): create controller

---
+++ app/backend/src/controllers/ContactController.ts
@@ -0,0 +1,104 @@
+import { Contact } from '@prisma/client';
+import { Request, Response, NextFunction } from 'express';
+import { RequestWithBody } from '../interfaces/RequestWithBody';
+import ContactService from '../services/ContactService';
+import HttpException from '../utils/exceptions/HttpException';
+
+export default class ContactController {
+  public route: string;
+
+  private service: ContactService;
+
+  constructor(
+    service: ContactService = new ContactService(),
+    route: string = '/contacts'
+  ) {
+    this.service = service;
+    this.route = route;
+  }
+
+  public create = async (
+    req: RequestWithBody<Contact>,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const { body } = req;
+    try {
+      const contact = await this.service.create(body, '');
+      return res.status(201).json({ contact });
+    } catch (err) {
+      if (err instanceof HttpException) {
+        return next(new HttpException(err.status, err.message));
+      }
+      next(new HttpException());
+    }
+  };
+
+  public read = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const { id } = req.body;
+    try {
+      const contacts = await this.service.list(id);
+      return res.status(200).json(contacts);
+    } catch (err) {
+      if (err instanceof HttpException) {
+        return next(new HttpException(err.status, err.message));
+      }
+      next(new HttpException());
+    }
+  };
+
+  public readOne = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const { id } = req.params;
+    try {
+      const contact = await this.service.listById(id);
+      return res.status(200).json({ contact });
+    } catch (err) {
+      if (err instanceof HttpException) {
+        return next(new HttpException(err.status, err.message));
+      }
+      next(new HttpException());
+    }
+  };
+
+  public update = async (
+    req: RequestWithBody<Contact>,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const { body } = req;
+    try {
+      const contact = await this.service.update(body);
+      return res.status(200).json({ contact });
+    } catch (err) {
+      if (err instanceof HttpException) {
+        return next(new HttpException(err.status, err.message));
+      }
+      next(new HttpException());
+    }
+  };
+
+  public delete = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const { id } = req.params;
+    try {
+      await this.service.destroy(id);
+      return res.status(204).end();
+    } catch (err) {
+      if (err instanceof HttpException) {
+        return next(new HttpException(err.status, err.message));
+      }
+      next(new HttpException());
+    }
+  };
+}
