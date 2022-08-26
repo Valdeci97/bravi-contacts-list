9ccd24e0e4909b1a08ba6d5fc1a03f550539a3e4
valdeci <moura97.valdeci@gmail.com>
2022-08-26 15:43:02 -0300

    feat(jsonwebtoken): create middleware

---
+++ app/backend/src/middlewares/Token.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import HttpException from '../utils/exceptions/HttpException';
+import { Token } from '../types/Token';
+import JsonWebToken from '../utils/jwt';
+
+export default class TokenMiddleware {
+  public validate(
+    req: Request,
+    _res: Response,
+    next: NextFunction
+  ): Response | void {
+    try {
+      const { authorization } = req.headers;
+      if (!authorization) {
+        return next(new HttpException(404, 'Token not found!'));
+      }
+      const token = JsonWebToken.decode(authorization);
+      const { id } = token as Token;
+      req.user = {
+        id,
+      };
+      next();
+    } catch (err) {
+      next(new HttpException(401, 'Invalid token!'));
+    }
+  }
+}
