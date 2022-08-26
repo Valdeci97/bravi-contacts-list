ca0cd49447ab50aeb0ed758225ff42a7ba7d948c
valdeci <moura97.valdeci@gmail.com>
2022-08-26 11:05:16 -0300

    feat(uuid): create middleware

---
+++ app/backend/src/middlewares/GuidMiddleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import { uuidSchema } from '../utils/joi-schemas/uuid';
+
+export default class GuidMiddleware {
+  public validateGuid = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { id } = req.params;
+    const { error } = uuidSchema.validate(id);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+}
