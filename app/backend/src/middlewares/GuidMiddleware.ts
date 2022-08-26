8f7df1e855f75f87735a8ad88706fbb02b1c308e
valdeci <moura97.valdeci@gmail.com>
2022-08-26 15:20:39 -0300

    feat(user): validate request body id

--- app/backend/src/middlewares/GuidMiddleware.ts
+++ app/backend/src/middlewares/GuidMiddleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { uuidSchema } from '../utils/joi-schemas/uuid';
+import { uuidBodySchema, uuidSchema } from '../utils/joi-schemas/uuid';
 
 export default class GuidMiddleware {
   public validateGuid = (
@@ -9,6 +9,19 @@ export default class GuidMiddleware {
   ): Response | void => {
     const { id } = req.params;
     const { error } = uuidSchema.validate(id);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+
+  public validateBodyGuid = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = uuidBodySchema.validate(req.user);
     if (error) {
       const [code, message] = error.message.split('/');
       return res.status(Number(code)).json({ message });
