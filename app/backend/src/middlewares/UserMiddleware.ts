c76dd67098a755826389bedf1ebe7b19de2495a3
valdeci <moura97.valdeci@gmail.com>
2022-08-25 23:24:56 -0300

    feat(user): create validation middleware

---
+++ app/backend/src/middlewares/UserMiddleware.ts
@@ -0,0 +1,75 @@
+import { Request, Response, NextFunction } from 'express';
+import {
+  userEmail,
+  username,
+  userOptionalEmail,
+  userOptionalPassword,
+  userPassword,
+} from '../utils/joi-schemas/user';
+
+export default class UserMiddleware {
+  public validateName = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = username.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+
+  public validateEmail = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userEmail.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+
+  public validatePassword = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userPassword.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+
+  public validateOptionalEmail = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userOptionalEmail.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+
+  public validateOptionalPassword = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userOptionalPassword.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+}
