3b0b31167b48748f8ab6e8db3ec1841711d9c7f6
valdeci <moura97.valdeci@gmail.com>
2022-08-25 22:06:32 -0300

    feat(contact): add routes to server

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,6 +1,7 @@
 import App from './app';
 import CustomRouter from './routes';
 import UserController from './controllers/UserController';
+import ContactController from './controllers/ContactController';
 
 const userController = new UserController();
 const userRouter = new CustomRouter();
@@ -14,8 +15,26 @@ userRouter.addPatchRoute(
 );
 userRouter.addDeleteRoute(`${userController.route}/:id`, userController.delete);
 
+const contactController = new ContactController();
+const contactRouter = new CustomRouter();
+
+contactRouter.addGetRoute(contactController.route, contactController.read);
+contactRouter.addGetRoute(
+  `${contactController.route}/:id`,
+  contactController.readOne
+);
+contactRouter.addPostRoute(contactController.route, contactController.create);
+contactRouter.addPutRoute(contactController.route, contactController.update);
+contactRouter.addDeleteRoute(
+  `${contactController.route}/:id`,
+  contactController.delete
+);
+
 const server = new App();
 
 server.addRouter(userRouter.router);
+server.addRouter(contactRouter.router);
+
+server.addErrorMiddleware();
 
 export default server;
