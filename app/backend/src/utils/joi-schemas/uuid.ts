6c5985032d6570bfc60a9c3311dfc4c8cdf23cd3
valdeci <moura97.valdeci@gmail.com>
2022-08-26 15:20:07 -0300

    feat(user): validate request body id

--- app/backend/src/utils/joi-schemas/uuid.ts
+++ app/backend/src/utils/joi-schemas/uuid.ts
@@ -1,7 +1,17 @@
 import joi from 'joi';
 
 const NOT_UUID_ID = '400/Id param must be an uuid string!';
+const NOT_AVAILABLE_BODY_ID = '400/id field must be filled!';
 
 export const uuidSchema = joi.string().guid().required().messages({
   'string.guid': NOT_UUID_ID,
 });
+
+export const uuidBodySchema = joi
+  .object({
+    id: joi.string().guid().required().messages({
+      'any.required': NOT_AVAILABLE_BODY_ID,
+      'string.guid': NOT_UUID_ID,
+    }),
+  })
+  .unknown(true);
